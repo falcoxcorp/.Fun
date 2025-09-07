ce3b2787707b8ce2da547aea2db688437f27171a
falcoxcorp <falcoxcorp@gmail.com>
2025-09-07 17:26:54 -0400

    Fix getDefaultConfig import from @rainbow-me/rainbowkit

--- src/wagmiClient.jsx
+++ src/wagmiClient.jsx
@@ -1,4 +1,4 @@
-import { getDefaultConfig } from '@rainbow-me/rainbowkit';
+import { getDefaultConfig } from '@rainbow-me/rainbowkit/config';
 import { http } from 'wagmi';
 import {bscTestnet, coreDao, soneium} from 'wagmi/chains';
 
